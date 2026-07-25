d3a8c61121e3992ae04dff629c5bdadc716b21a5
Ndukwe Daniel <pomiledaniel@gmail.com>
2026-07-25 20:47:06 +0100

    feat: create User entity with BankAccount relationship

---
+++ src/main/java/com/fintech/zend/model/User.java
@@ -0,0 +1,119 @@
+package com.fintech.zend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String accountNumber;
+
+    @Column(unique = true, nullable = false)
+    private String phoneNumber;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "bank_account_id")
+    private BankAccount bankAccount;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String username, String accountNumber, String phoneNumber,
+            String email, String password, BankAccount bankAccount) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.accountNumber = accountNumber;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.password = password;
+        this.bankAccount = bankAccount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public BankAccount getBankAccount() {
+        return bankAccount;
+    }
+
+    public void setBankAccount(BankAccount bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+}
