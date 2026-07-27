9a104a2dfe880160f741336645e7de812f6034b6
Ndukwe Daniel <pomiledaniel@gmail.com>
2026-07-27 03:07:28 +0100

    Removed the inital createAccount to create a better one for the actual bank Infra

---
+++ src/main/java/com/fintech/zend/controller/AuthController.java
@@ -0,0 +1,46 @@
+package com.fintech.zend.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fintech.zend.dto.SignupRequest;
+import com.fintech.zend.dto.SignupResponse;
+import com.fintech.zend.dto.LoginRequest;
+import com.fintech.zend.service.AuthService;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<SignupResponse> signup(@RequestBody SignupRequest request) {
+        try {
+            SignupResponse response = authService.signup(request);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+        } catch (IllegalArgumentException e) {
+                SignupResponse response = new SignupResponse(e.getMessage(), null, null);
+                return ResponseEntity.badRequest().body(response);
+
+        }
+    }
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest request) {
+
+        authService.login(
+                request.getemailorAccountNumber(),
+                request.getPassword());
+
+        return ResponseEntity.ok("Login successful.");
+    }
+}
