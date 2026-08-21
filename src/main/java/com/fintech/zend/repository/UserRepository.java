7e000818c0b3325b88c1ebfba191313fcaa50bed
Ndukwe Daniel <pomiledaniel@gmail.com>
2026-08-21 17:06:07 +0100

    This methods defined in this interface allows querying users by their properties

--- src/main/java/com/fintech/zend/repository/UserRepository.java
+++ src/main/java/com/fintech/zend/repository/UserRepository.java
@@ -1,12 +1,14 @@
 package com.fintech.zend.repository;
 
 import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.fintech.zend.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
-    Optional<User> findByAccountNumber(String accountNumber);
+    Optional<User> findByBankAccount_AccountNumber(String accountNumber);   
     Optional<User> findByEmail(String email);
     Optional<User> findByPhoneNumber(String phoneNumber);
 }
