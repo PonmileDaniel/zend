78da1b910b1a668a23b73cd0ddde6c22b76fa81c
Ndukwe Daniel <pomiledaniel@gmail.com>
2026-08-21 17:08:08 +0100

    This ckass has a contructor that initializes all the properties the getter and setter methods for each property

--- src/main/java/com/fintech/zend/model/User.java
+++ src/main/java/com/fintech/zend/model/User.java
@@ -1,6 +1,15 @@
 package com.fintech.zend.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
 
 @Entity
 @Table(name = "users")
@@ -19,9 +28,6 @@ public class User {
     private String username;
 
     @Column(unique = true, nullable = false)
-    private String accountNumber;
-
-    @Column(unique = true, nullable = false)
     private String phoneNumber;
 
     @Column(unique = true, nullable = false)
@@ -37,12 +43,11 @@ public class User {
     public User() {
     }
 
-    public User(String firstName, String lastName, String username, String accountNumber, String phoneNumber,
+    public User(String firstName, String lastName, String username, String phoneNumber,
             String email, String password, BankAccount bankAccount) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.username = username;
-        this.accountNumber = accountNumber;
         this.phoneNumber = phoneNumber;
         this.email = email;
         this.password = password;
@@ -75,14 +80,6 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(String accountNumber) {
-        this.accountNumber = accountNumber;
     }
 
     public String getPhoneNumber() {
