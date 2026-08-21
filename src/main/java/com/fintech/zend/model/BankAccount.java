691a023626c6a562562766a3106fb7d124167213
Ndukwe Daniel <pomiledaniel@gmail.com>
2026-08-21 17:21:19 +0100

    to differenttiate one bank from the other or an account

--- src/main/java/com/fintech/zend/model/BankAccount.java
+++ src/main/java/com/fintech/zend/model/BankAccount.java
@@ -1,5 +1,7 @@
 package com.fintech.zend.model;
 
+import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,16 +12,15 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-
-
 
 @Entity
 @Table(name = "bank_accounts")
 
-public class BankAccount {
+public class BankAccount implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -27,24 +28,20 @@ public class BankAccount {
     @Column(unique = true, nullable = false)
     private String accountNumber;
 
-    @Column(nullable = false)
-    private String holderName;
-
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinColumn(name = "account_number", referencedColumnName = "accountNumber")
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "bankAccount", orphanRemoval = true)
     private List<Transaction> transactions = new ArrayList<>();
 
-    private double balance;
-
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal balance;
 
     public BankAccount() {
     }
 
     // Constructor
-    public BankAccount(String accountNumber, String holderName) {
+    public BankAccount(String accountNumber) {
         this.accountNumber = accountNumber;
-        this.holderName = holderName;
-        this.balance = 0.0;
+        // this.holderName = holderName;
+        this.balance = BigDecimal.ZERO;
     }
 
     public Long getId() {
@@ -63,19 +60,11 @@ public class BankAccount {
         this.accountNumber = accountNumber;
     }
 
-    public String getHolderName() {
-        return holderName;
-    }
-
-    public void setHolderName(String holderName) {
-        this.holderName = holderName;
-    }
-
-    public double getBalance() {
+    public BigDecimal getBalance() {
         return balance;
     }
 
-    public void setBalance(double balance) {
+    public void setBalance(BigDecimal balance) {
         this.balance = balance;
     }
 
@@ -87,51 +76,71 @@ public class BankAccount {
         this.transactions = transactions;
     }
 
-    public void addTransaction(String description, double amount) {
+    public void addTransaction(String description, BigDecimal amount, TransactionType transactionType,
+            TransactionDirection direction, String reference) {
         Transaction tx = new Transaction();
         tx.setBankAccount(this);
         tx.setDescription(description);
+        tx.setReference(reference);
         tx.setAmount(amount);
         tx.setTimestamp(java.time.LocalDateTime.now());
+        tx.setTransactionType(transactionType);
+        tx.setDirection(direction);
+        tx.setStatus(TransactionStatus.SUCCESS);
+        // tx.setBank("ZEND");
         this.transactions.add(tx);
     }
 
-
-    public void deposit(double amount) {
-        if (amount <= 0) {
+    public void deposit(BigDecimal amount, String reference) {
+        if (amount == null || amount.compareTo(BigDecimal.ZERO) <= 0) {
             throw new IllegalArgumentException("Deposit amount must be greater than zero.");
         }
-        this.balance += amount;
-        addTransaction("Deposited: +" + String.format("%.2f", amount), amount);
+        this.balance = this.balance.add(amount);
+        addTransaction("Deposit ", amount, TransactionType.DEPOSIT, TransactionDirection.INWARD, reference);
     }
 
-    public void withdraw(double amount) throws InsufficientFundsException {
-        if (amount <= 0) {
+    public void withdraw(BigDecimal amount, String reference) throws InsufficientFundsException {
+        if (amount == null || amount.compareTo(BigDecimal.ZERO) <= 0) {
             throw new IllegalArgumentException("Withdrawal amount must be positive.");
         }
-        if (amount > balance) {
-            throw new InsufficientFundsException("Insufficient funds. Balance: " + String.format("%.2f", balance));
+        if (amount.compareTo(balance) > 0) {
+            throw new InsufficientFundsException("Insufficient funds. Balance: " + balance);
         }
-        this.balance -= amount;
-        addTransaction("Withdrew: -" + String.format("%.2f", amount), -amount);
+        this.balance = this.balance.subtract(amount);
+        addTransaction("Transfer", amount, TransactionType.TRANSFER, TransactionDirection.OUTWARD, reference);
+    }
+
+    public void receiveTransfer(
+            BigDecimal amount,
+            String description, String reference) {
+
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException(
+                    "Transfer amount must be greater than zero.");
+        }
+
+        this.balance = this.balance.add(amount);
+
+        addTransaction(
+                description,
+                amount,
+                TransactionType.TRANSFER,
+                TransactionDirection.INWARD, reference);
     }
 
     public void printStatement() {
         for (Transaction tx : transactions) {
             System.out.println(
-                tx.getTimestamp() + " | " +
-                tx.getDescription() + " | " +
-                tx.getAmount()
-            );
+                    tx.getTimestamp() + " | " +
+                            tx.getDescription() + " | " +
+                            tx.getAmount());
         }
     }
-
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("Account: ").append(accountNumber)
-                .append(" | Holder: ").append(holderName)
                 .append(" | Balance: ").append(String.format("%.2f", balance)).append("\n");
         sb.append("Transactions:\n");
         for (Transaction tx : transactions) {
