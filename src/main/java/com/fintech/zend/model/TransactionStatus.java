502fd271669628b63dde98795d6c09e68478fd0f
Ndukwe Daniel <pomiledaniel@gmail.com>
2026-08-21 17:09:58 +0100

    Enum for transaction status

---
+++ src/main/java/com/fintech/zend/model/TransactionStatus.java
@@ -0,0 +1,12 @@
+/**
+ * Enum to represent the status of a transaction.
+ * SUCCESS means the transaction was successful,
+ * FAILED means the transaction failed.
+ */
+package com.fintech.zend.model;
+
+public enum TransactionStatus {
+    SUCCESS,
+    FAILED
+}
+
