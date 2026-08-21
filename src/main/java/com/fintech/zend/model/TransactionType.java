2288f7f817746b7d9384a4d0820a9279a6766724
Ndukwe Daniel <pomiledaniel@gmail.com>
2026-08-21 17:11:14 +0100

    This is the transasction type if it is a Deposit or Transfer

---
+++ src/main/java/com/fintech/zend/model/TransactionType.java
@@ -0,0 +1,12 @@
+/**
+ * Enum to represent the type of a transaction.
+ * DEPOSIT means the transaction is a deposit (i.e. adding funds to an account)
+ * TRANSFER means the transaction is a transfer (i.e. moving funds from one account to another)
+ */
+package com.fintech.zend.model;
+
+public enum TransactionType {
+    DEPOSIT,
+    TRANSFER 
+}
+
