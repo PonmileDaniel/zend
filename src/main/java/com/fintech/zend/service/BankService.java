aa321da59c32c76cfc5edaef700cdbd532382523
Ndukwe Daniel <pomiledaniel@gmail.com>
2026-08-21 17:01:28 +0100

    This include deposit, transfer, statement and genereationTranRef and also some commented out code for future purpose

--- src/main/java/com/fintech/zend/service/BankService.java
+++ src/main/java/com/fintech/zend/service/BankService.java
@@ -1,8 +1,9 @@
 package com.fintech.zend.service;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
-import java.util.Random;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,86 +11,110 @@ import org.springframework.transaction.annotation.Transactional;
 import com.fintech.zend.model.BankAccount;
 import com.fintech.zend.model.InsufficientFundsException;
 import com.fintech.zend.repository.BankAccountRepository;
+import com.fintech.zend.security.SecurityUtils;
+import com.fintech.zend.security.SessionPrincipal;
 
 @Service
 public class BankService {
 
     private final BankAccountRepository accountRepository;
-    private final Random random = new Random();
 
     public BankService(BankAccountRepository accountRepository) {
         this.accountRepository = accountRepository;
     }
 
-    public void createAccount(String holderName) {
-        String accNum = generateAccountNumber();
-        BankAccount newAccount = new BankAccount(accNum, holderName);
-        accountRepository.save(newAccount);
-    }
-
-    private String generateAccountNumber() {
-        String accountNumber;
-
-        do {
-            long number = 1_000_000_000l + (long) (random.nextDouble() * 9_000_000_000l);
-            accountNumber = String.valueOf(number);
-        } while (accountRepository.findByAccountNumber(accountNumber).isPresent());
-        return accountNumber;
-    }
-
-
-
+    /**
+     * Transfer money from one account to another.
+     * 
+     * @param fromNum The account number to transfer from.
+     * @param toNum   The account number to transfer to.
+     * @param amount  The amount of money to transfer.
+     * 
+     * @throws SecurityException        If the user is not authorized to transfer
+     *                                  from the given account.
+     * @throws IllegalArgumentException If the sender or receiver account is not
+     *                                  found, or if the sender and receiver are the
+     *                                  same account.
+     * @throws IllegalStateException    If the sender account does not have
+     *                                  sufficient funds to make the transfer.
+     */
     @Transactional
-    public void transfer(String fromNum, String toNum, double amount) {
+    public void transfer(String fromNum, String toNum, BigDecimal amount, String description) {
+        SessionPrincipal user = SecurityUtils.getCurrentUser();
+
+        if (!user.getAccountNumber().equals(fromNum)) {
+            throw new SecurityException("You are not authorized to transfer from this account");
+        }
         BankAccount from = accountRepository.findByAccountNumber(fromNum)
-                .orElseThrow(() -> new IllegalArgumentException("Receiver not found"));
+                .orElseThrow(() -> new IllegalArgumentException("Sender not found"));
         BankAccount to = accountRepository.findByAccountNumber(toNum)
                 .orElseThrow(() -> new IllegalArgumentException("Receiver not found"));
 
-        // Use the withdraw/deposit method
+        if (fromNum.equals(toNum)) {
+            throw new IllegalArgumentException("Cannot transfer to the same account");
+        }
+
+        String reference = generateTransactionReference();
         try {
-            from.withdraw(amount);
+            from.withdraw(amount, reference);
+
+            to.receiveTransfer(amount, description, reference);
         } catch (InsufficientFundsException e) {
             throw new IllegalStateException(e.getMessage());
         }
-        to.deposit(amount);
 
         accountRepository.save(from);
         accountRepository.save(to);
     }
 
+    /**
+     * Deposits money into a bank account.
+     * 
+     * @param accountNumber The account number to deposit into.
+     * @param amount The amount of money to deposit.
+     * 
+     * @throws SecurityException If the user is not authorized to deposit into the given account.
+     * @throws IllegalArgumentException If the account is not found.
+     */
     @Transactional
-    public void deposit(String accountNumber, double amount) {
+    public void deposit(String accountNumber, BigDecimal amount) {
+        SessionPrincipal user = SecurityUtils.getCurrentUser();
+        String reference = generateTransactionReference();
+
+        if (!user.getAccountNumber().equals(accountNumber)) {
+            throw new SecurityException("You are not authorized to deposit into this account");
+        }
         BankAccount account = accountRepository.findByAccountNumber(accountNumber)
                 .orElseThrow(() -> new IllegalArgumentException("Account not found"));
-        account.deposit(amount);
+        account.deposit(amount, reference);
         accountRepository.save(account);
     }
 
+    /**
+     * Retrieves the statement of a bank account.
+     * 
+     * @param accountNumber the account number of the account to retrieve the
+     *                      statement for
+     * @return a BankAccount object if the request was successful, or an error
+     *         message if the request failed
+     * 
+     * @throws SecurityException if the authenticated user is not authorized to
+     *                           access the account
+     */
     public BankAccount getStatement(String accountNumber) {
+        SessionPrincipal user = SecurityUtils.getCurrentUser();
+
+        if (!user.getAccountNumber().equals(accountNumber)) {
+            throw new SecurityException("You are not authorized to access the account");
+        }
         return accountRepository.findByAccountNumber(accountNumber)
                 .orElseThrow(() -> new IllegalArgumentException("Account not found"));
     }
 
-    public String getHolderName(String accountNumber) {
-        BankAccount account = accountRepository.findByAccountNumber(accountNumber).orElseThrow(() -> new IllegalArgumentException("Account not found"));
-        return account.getHolderName();
+    private String generateTransactionReference() {
+        return "TXN-" + UUID.randomUUID().toString().replace("-", "").toUpperCase();
     }
 
-    public List<BankAccount> getAccounts() {
-        return accountRepository.findAll();
-    }
-
-    public void printAllAccounts() {
-        System.out.println("\n=== All Accounts ===");
-        List<BankAccount> accounts = accountRepository.findAll();
-        if (accounts.isEmpty()) {
-            System.out.println("No accounts to display.");
-            return;
-        }
-        accounts.stream().forEach(acc -> System.out.println(acc.getAccountNumber() + " | " + acc.getHolderName()
-                + " | Balance: " + String.format("%.2f", acc.getBalance())));
-    }
 
     public void removeAccount(String accountNumber) {
         BankAccount account = accountRepository.findByAccountNumber(accountNumber)
@@ -112,20 +137,44 @@ public class BankService {
         return accountRepository.findByAccountNumber(accountNumber);
     }
 
-    public double getTotalBalance() {
-        return accountRepository.findAll().stream().mapToDouble(BankAccount::getBalance).sum();
+    public BigDecimal getTotalBalance() {
+        return accountRepository.findAll().stream().map(BankAccount::getBalance).reduce(BigDecimal.ZERO,
+                BigDecimal::add);
     }
 
-    public void printAccountNames() {
-        accountRepository.findAll().stream().map(BankAccount::getHolderName).forEach(System.out::println);
-    }
+    // public void printAccountNames() {
+    // accountRepository.findAll().stream().map(BankAccount::getHolderName).forEach(System.out::println);
+    // }
 
-    public void printHighBalanceAccounts(double minimumBalance) {
-        accountRepository.findAll().stream()
-                .filter(account -> account.getBalance() >= minimumBalance)
-                .forEach(acc -> System.out.println(
-                        acc.getAccountNumber() + " | " +
-                                acc.getHolderName() + " | Balance: " +
-                                String.format("%.2f", acc.getBalance())));
-    }
+    // public void printHighBalanceAccounts(double minimumBalance) {
+    // accountRepository.findAll().stream()
+    // .filter(account -> account.getBalance() >= minimumBalance)
+    // .forEach(acc -> System.out.println(
+    // acc.getAccountNumber() + " | " +
+    // acc.getHolderName() + " | Balance: " +
+    // String.format("%.2f", acc.getBalance())));
+    // }
+
+     // public void printAllAccounts() {
+    // System.out.println("\n=== All Accounts ===");
+    // List<BankAccount> accounts = accountRepository.findAll();
+    // if (accounts.isEmpty()) {
+    // System.out.println("No accounts to display.");
+    // return;
+    // }
+    // accounts.stream().forEach(acc -> System.out.println(acc.getAccountNumber() +
+    // " | " + acc.getHolderName()
+    // + " | Balance: " + String.format("%.2f", acc.getBalance())));
+    // }
+
+
+    // public String getHolderName(String accountNumber) {
+    // BankAccount account = accountRepository.findByAccountNumber(accountNumber)
+    // .orElseThrow(() -> new IllegalArgumentException("Account not found"));
+    // return account.getHolderName();
+    // }
+
+    // public List<BankAccount> getAccounts() {
+    //     return accountRepository.findAll();
+    // }
 }
