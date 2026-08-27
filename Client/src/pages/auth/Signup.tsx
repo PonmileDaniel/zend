818b4ea682e35f3cea0ff5e35c868205b96ea5ef
Ndukwe Daniel <pomiledaniel@gmail.com>
2026-08-27 21:56:36 +0100

    Signup page of the Finance application

---
+++ Client/src/pages/auth/Signup.tsx
@@ -0,0 +1,12 @@
+export default function Signup() {
+    return (
+        <main className="min-h-screen bg-[#131313] text-white">
+            <div className="flex min-h-screen flex-col lg:flex-row">
+
+                {/**LEFT - SIGNUP FORM */}
+                <section className=""></section>
+            </div>
+        </main>
+
+    )
+}
