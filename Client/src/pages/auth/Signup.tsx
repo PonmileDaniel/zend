7a24576597423932d5bcec5d0cf73f6068a7c01e
Ndukwe Daniel <pomiledaniel@gmail.com>
2026-08-27 22:04:43 +0100

    Worked on the header of the Signup page

--- Client/src/pages/auth/Signup.tsx
+++ Client/src/pages/auth/Signup.tsx
@@ -4,7 +4,12 @@ export default function Signup() {
             <div className="flex min-h-screen flex-col lg:flex-row">
 
                 {/**LEFT - SIGNUP FORM */}
-                <section className=""></section>
+                <section className="flex min-h screen w-full flex-col border-b border-[#262626] lg:w-1/2 lg:border-b-0 lg:border-r">
+                    {/** Header */}
+                    <header className="flex item-center px-6 py-8 lg:px-12">
+                        <span className="text-3xl font-extrabold tracking-[-0.08em]">ZEND</span>
+                    </header>
+                </section>
             </div>
         </main>
 
