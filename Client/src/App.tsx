e1a4d7278cbbf5eb8d683e340c31b01da41655af
Ndukwe Daniel <pomiledaniel@gmail.com>
2026-08-28 21:02:22 +0100

    Layout for my Client

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
 import Splash from "./pages/Splash";
+import Signup from "./pages/auth/Signup";
+import Login from "./pages/auth/Login";
 
 function App() {
-  return <Splash />;
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 1800);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return showSplash ? <Splash /> : <Login />;
 }
 
 export default App;
