66007e86264f3e2ccba3cd53445ed4c91fbc1ec0
Ndukwe Daniel <pomiledaniel@gmail.com>
2026-08-31 20:48:42 +0100

    Reuseable component for the Header

---
+++ Client/src/components/auth/AuthHeader.tsx
@@ -0,0 +1,7 @@
+export default function AuthHeader() {
+  return (
+    <header className="flex shrink-0 items-center px-6 py-5 lg:px-12 lg:py-6">
+      <span className="text-3xl font-extrabold tracking-[-0.08em]">ZEND</span>
+    </header>
+  );
+}
