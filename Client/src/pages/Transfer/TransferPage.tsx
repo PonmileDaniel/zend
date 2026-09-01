fcdf8e3bb99d9d81c125a1f6714e960d85a6d30f
Ndukwe Daniel <pomiledaniel@gmail.com>
2026-09-01 22:14:04 +0100

    Prettier- Structure

--- Client/src/pages/Transfer/TransferPage.tsx
+++ Client/src/pages/Transfer/TransferPage.tsx
@@ -10,48 +10,35 @@ export default function TransferPage() {
   return (
     <main className="min-h-screen bg-[#131313] text-white">
       <div className="flex min-h-screen">
-
         {/* SIDEBAR */}
         <Sidebar
           isOpen={isSidebarOpen}
-          onToggle={() =>
-            setIsSidebarOpen((previous) => !previous)
-          }
+          onToggle={() => setIsSidebarOpen((previous) => !previous)}
           activeItem="Send"
         />
 
         {/* MAIN */}
         <div className="flex min-w-0 flex-1 flex-col">
-
           {/* HEADER */}
-          <DashboardHeader title="Send"/>
+          <DashboardHeader title="Send" />
 
           {/* CONTENT */}
           <div className="flex-1 overflow-y-auto">
-
             <div className="mx-auto w-full max-w-[1250px] px-5 py-6 lg:px-8 lg:py-8">
-
               <div className="grid grid-cols-1 gap-6 xl:grid-cols-[minmax(0,1fr)_300px]">
-
                 {/* LEFT */}
                 <div className="min-w-0">
                   <Transfer />
                 </div>
 
-
                 {/* RIGHT */}
                 <aside className="hidden xl:block">
                   <ThisMonth />
                 </aside>
-
               </div>
-
             </div>
-
           </div>
-
         </div>
-
       </div>
     </main>
   );
