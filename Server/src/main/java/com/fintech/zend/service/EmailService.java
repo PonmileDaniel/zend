bdf206cfcad1c984b8e25a71bb9df138179c468b
Ndukwe Daniel <pomiledaniel@gmail.com>
2026-09-12 13:43:27 +0100

    Addition of the email servie

--- Server/src/main/java/com/fintech/zend/service/EmailService.java
+++ Server/src/main/java/com/fintech/zend/service/EmailService.java
@@ -4,10 +4,9 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class EmailService {
-    
+
     private final JavaMailSender mailSender;
 
     public EmailService(JavaMailSender mailSender) {
@@ -20,12 +19,11 @@ public class EmailService {
         message.setTo(email);
         message.setSubject("Your Zend verification Code");
         message.setText(
-            "Your ZEND verification code is: " + otp
+                "Your ZEND verification code is: " + otp
                         + "\n\n"
                         + "This code will expire in 5 minutes."
                         + "\n\n"
-                        + "If you did not create a ZEND account, please ignore this email."
-        );
+                        + "If you did not create a ZEND account, please ignore this email.");
         mailSender.send(message);
     }
 }
