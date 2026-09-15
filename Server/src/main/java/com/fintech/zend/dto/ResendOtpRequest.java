61f5c70f8dd4ac2557e97080d996bea423a58985
Ndukwe Daniel <pomiledaniel@gmail.com>
2026-09-15 21:02:36 +0100

    Dto for the resendOtpresponse

---
+++ Server/src/main/java/com/fintech/zend/dto/ResendOtpRequest.java
@@ -0,0 +1,13 @@
+package com.fintech.zend.dto;
+
+public class ResendOtpRequest {
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
