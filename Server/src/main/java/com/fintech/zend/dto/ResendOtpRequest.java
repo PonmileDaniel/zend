de05c2c8dc3c82f6177bfcff531ae50df4e508be
Ndukwe Daniel <pomiledaniel@gmail.com>
2026-09-20 23:44:47 +0100

    feat: Worked on a functionality to mask user email and instead send a challengeId to the client and also the masked mail, challengeId is then used as the look up to map back the email in redis.

--- Server/src/main/java/com/fintech/zend/dto/ResendOtpRequest.java
+++ Server/src/main/java/com/fintech/zend/dto/ResendOtpRequest.java
@@ -2,12 +2,22 @@ package com.fintech.zend.dto;
 
 public class ResendOtpRequest {
     private String email;
+    private String challengeId;
 
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
-    
+
+    public String getChallengeId() {
+        return challengeId;
+    }
+
+    public void setChallengeId(String challengeId) {
+        this.challengeId = challengeId;
+    }
+
 }
