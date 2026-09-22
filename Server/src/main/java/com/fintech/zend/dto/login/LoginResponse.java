53bca9a9a9481445cc7454e25ac51cde9cca453e
Ndukwe Daniel <pomiledaniel@gmail.com>
2026-09-23 00:59:55 +0100

    issue: Reorganisation of the file folder

---
+++ Server/src/main/java/com/fintech/zend/dto/login/LoginResponse.java
@@ -0,0 +1,37 @@
+package com.fintech.zend.dto.login;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class LoginResponse {
+
+    private final String message;
+    private String challengeId;
+    private String maskedEmail;
+
+    public LoginResponse(String message) {
+        this.message = message;
+    }
+
+    public LoginResponse(
+            String message,
+            String challengeId,
+            String maskedEmail) {
+
+        this.message = message;
+        this.challengeId = challengeId;
+        this.maskedEmail = maskedEmail;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getChallengeId() {
+        return challengeId;
+    }
+
+    public String getMaskedEmail() {
+        return maskedEmail;
+    }
+}
