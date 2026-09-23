0d0dfe21917fced6ad42bd831b474b267436dd04
Ndukwe Daniel <pomiledaniel@gmail.com>
2026-09-23 21:06:41 +0100

    fix: Added the API_URL_DASHBOARD url to the api.ts file

--- Client/src/lib/api.ts
+++ Client/src/lib/api.ts
@@ -1,4 +1,8 @@
 const API_URL =
   import.meta.env.VITE_API_URL ?? "http://localhost:8081/api/auth";
 
-export default API_URL;
+const API_URL_DASHBOARD =
+  import.meta.env.VITE_API_URL_DASHBOARD ??
+  "http://localhost:8081/api/dashboard";
+
+export { API_URL, API_URL_DASHBOARD };
