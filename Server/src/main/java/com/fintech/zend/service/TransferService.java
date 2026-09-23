e05a3e3cdd131d578102ca5a2a8af2b73c16b758
Ndukwe Daniel <pomiledaniel@gmail.com>
2026-09-23 23:07:28 +0100

    feat: Transfer service

--- Server/src/main/java/com/fintech/zend/service/TransferService.java
+++ Server/src/main/java/com/fintech/zend/service/TransferService.java
@@ -1,5 +1,13 @@
 package com.fintech.zend.service;
 
+
+import com.fintech.zend.dto.RecipientResponse;
+import com.fintech.zend.model.BankAccount;
+import com.fintech.zend.model.User;
+import com.fintech.zend.repository.BankAccountRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TransferService {
     
 }
