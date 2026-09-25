e2d5245ac5411d59740ac0a324a47b548befc263
Ndukwe Daniel <pomiledaniel@gmail.com>
2026-09-25 22:28:02 +0100

    feat: Retrieval of user name when an account is searched

--- Client/src/lib/api.ts
+++ Client/src/lib/api.ts
@@ -5,4 +5,6 @@ const API_URL_DASHBOARD =
   import.meta.env.VITE_API_URL_DASHBOARD ??
   "http://localhost:8081/api/dashboard";
 
-export { API_URL, API_URL_DASHBOARD };
+const API_URL_TRANSFER = import.meta.env.VITE_API_URL_TRANSFER ?? "http://localhost:8081/api/transfer";
+
+export { API_URL, API_URL_DASHBOARD, API_URL_TRANSFER };
